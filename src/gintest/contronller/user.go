bb6d02a71ba637bca400ccf5b39211cc9282e21a
mwei314 <mwei2358@163.com>
2020-04-09 21:11:48 +0800

    增加参数验证

--- src/gintest/contronller/user.go
+++ src/gintest/contronller/user.go
@@ -14,8 +14,18 @@ func UserList(c *gin.Context) {
 	c.String(http.StatusOK, "hello world")
 }
 
+type addUserValid struct {
+	name string `form:"name" binding:"required"`
+	pass string `form:"pass" binding:"required"`
+}
+
 // AddUser 新增用户
 func AddUser(c *gin.Context) {
+	var userValid addUserValid
+	if err := c.ShouldBind(userValid); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	name := c.PostForm("name")
 	pass := c.PostForm("pass")
 	user := &models.User{
@@ -25,6 +35,7 @@ func AddUser(c *gin.Context) {
 	err := user.Insert()
 	if err != nil {
 		c.AbortWithError(501, err)
+		return
 	}
 	c.AbortWithStatusJSON(200, user)
 }
